f4ead8907ad97762f4fac679d292fdf834c45ef6
lan9liang <lan9liang@163.com>
2016-10-12 15:18:26 +0800

    测试主类

---
+++ src/com/test/TestMain.java
@@ -0,0 +1,15 @@
+package com.test;
+/**
+* Title: TestMain
+* Company: sjy
+* @author 兰小刚    QQ:757553326
+* @date 2016年10月12日
+*类说明：TestMain
+*/
+public class TestMain {
+
+	public static void main(String[] args) {
+		System.out.println("JohnLan是个大帅哥");
+	}
+
+}
